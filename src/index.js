6ec7f296cb98681280cf65b29cb21cf512c987b1
TaeChan <xocksdlekd@gmail.com>
2020-01-22 05:08:27 +0900

    redux 사용 예제 작성

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDom from 'react-dom';
 
 import App from './components/App.js';
 
-ReactDom.render(
+import { createStore } from 'redux';
+import reducers from './reducers';
 
-  <App/>,
+import * as actions from './actions';
+
+import { Provider } from 'react-redux';
+
+const store = createStore(reducers);
+
+ReactDom.render(
+  <Provider store={store }>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 );
