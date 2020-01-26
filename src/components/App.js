afea8bea2017eff15992f38acfec3ed0bf254c4a
TaeChan <xocksdlekd@gmail.com>
2020-01-27 04:18:48 +0900

    rest api 호출해서 데이터 가져오기

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,39 @@ const propTypes = {
 };
 const defaultProps = {
 };
+
+	
+
 class App extends Component {
     constructor(props) {
         super(props);
+		this.state = {
+			data: ''
+		};
+
+		this.callApi = this.callApi.bind(this);
     }
+
+	//api 테스트 로직
+	callApi(){
+    fetch("http://localhost:8081/rest")
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          data: json.test
+        })
+      })
+  }
+
+  componentDidMount() {
+    this.callApi();
+  }
+
     render() {
         return(
             <div>
               <Counter/>
+			  <h3>{this.state.data ? this.state.data : '데이터 불러오는 중'}</h3>
             </div>
         );
     }
