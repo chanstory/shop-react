57b787222ceae10886a87f0e4d8597ec963f10d4
TaeChan <xocksdlekd@gmail.com>
2020-04-03 17:52:08 +0900

    ie11, edge 크로스 브라우징 문제 해결중..

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
 import React from 'react';
 import ReactDom from 'react-dom';
+
 
 import App from './components/App.js';
 
